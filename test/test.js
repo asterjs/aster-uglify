5863c465bef09b4b9f5ba725e24a90ce00762e7e
mikach <mikach27@gmail.com>
2014-06-04 01:04:21 +0300

    Add `loc` to AST nodes

--- test/test.js
+++ test/test.js
@@ -14,15 +14,55 @@ it('should minify', function (done) {
 				"type": "Program",
 				"body": [
 					{
+						"loc": {
+							"start": {
+								"line": 2,
+								"column": 0
+							},
+							"end": {
+								"line": 2,
+								"column": 12
+							}
+						},
 						"type": "VariableDeclaration",
 						"declarations": [
 							{
+								"loc": {
+									"start": {
+										"line": 2,
+										"column": 4
+									},
+									"end": {
+										"line": 2,
+										"column": 11
+									}
+								},
 								"type": "VariableDeclarator",
 								"id": {
+									"loc": {
+										"start": {
+											"line": 2,
+											"column": 4
+										},
+										"end": {
+											"line": 2,
+											"column": 7
+										}
+									},
 									"type": "Identifier",
 									"name": "aaa"
 								},
 								"init": {
+									"loc": {
+										"start": {
+											"line": 2,
+											"column": 10
+										},
+										"end": {
+											"line": 2,
+											"column": 11
+										}
+									},
 									"type": "Literal",
 									"value": 1,
 									"raw": "1"
@@ -32,22 +72,82 @@ it('should minify', function (done) {
 						"kind": "var"
 					},
 					{
+						"loc": {
+							"start": {
+								"line": 3,
+								"column": 0
+							},
+							"end": {
+								"line": 3,
+								"column": 19
+							}
+						},
 						"type": "IfStatement",
 						"test": {
+							"loc": {
+								"start": {
+									"line": 3,
+									"column": 4
+								},
+								"end": {
+									"line": 3,
+									"column": 9
+								}
+							},
 							"type": "Literal",
 							"value": false,
 							"raw": "false"
 						},
 						"consequent": {
+							"loc": {
+								"start": {
+									"line": 3,
+									"column": 11
+								},
+								"end": {
+									"line": 3,
+									"column": 19
+								}
+							},
 							"type": "ExpressionStatement",
 							"expression": {
+								"loc": {
+									"start": {
+										"line": 3,
+										"column": 11
+									},
+									"end": {
+										"line": 3,
+										"column": 18
+									}
+								},
 								"type": "AssignmentExpression",
 								"operator": "=",
 								"left": {
+									"loc": {
+										"start": {
+											"line": 3,
+											"column": 11
+										},
+										"end": {
+											"line": 3,
+											"column": 14
+										}
+									},
 									"type": "Identifier",
 									"name": "aaa"
 								},
 								"right": {
+									"loc": {
+										"start": {
+											"line": 3,
+											"column": 17
+										},
+										"end": {
+											"line": 3,
+											"column": 18
+										}
+									},
 									"type": "Literal",
 									"value": 2,
 									"raw": "2"
@@ -94,7 +194,6 @@ it('should minify', function (done) {
 
 	// simulating file sequence and applying transformation
 	uglify({
-		warnings : false,
 		mangle: true,
 		compress: {
 			warnings : false
